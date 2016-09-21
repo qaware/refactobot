2007c8b5d92f9fa66708900e4e1d11332e496f3c
Alexander Krauss <alexander.krauss@qaware.de>
2016-09-21 10:04:00 +0200

    Added test that demonstrates the use of javaparser api.

---
+++ src/test/resources/TestClass.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestClass {
+
+    private List<String> geeting = new ArrayList<>();
+
+    public TestClass() {
+        greeting.add("Hello");
+        greeting.add("World");
+    }
+
+}
