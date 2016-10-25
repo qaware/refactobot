1885fda77efdce37b39d07c7be278fe233795e26
Florian Engel <florian.engel@qaware.de>
2016-10-25 14:09:32 +0200

    Adds basic implementation of MavenScanner to bulk rename tool.

--- src/main/kotlin/de/qaware/tools/bulkrename/scanner/MavenScanner.kt
+++ src/main/kotlin/de/qaware/tools/bulkrename/scanner/MavenScanner.kt
@@ -1,10 +1,17 @@
 package de.qaware.tools.bulkrename.scanner
 
+import com.github.javaparser.JavaParser
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter
 import de.qaware.tools.bulkrename.model.codebase.Codebase
+import de.qaware.tools.bulkrename.model.codebase.File
+import de.qaware.tools.bulkrename.model.codebase.FileType
 import de.qaware.tools.bulkrename.model.codebase.Module
+import java.io.FileInputStream
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.util.*
 import java.util.function.BiPredicate
 import java.util.stream.Collectors
 
@@ -12,32 +19,110 @@ import java.util.stream.Collectors
  * Simple project scanner that uses maven directory structures.
  *
  * @author Alexander Krauss alexander.krauss@qaware.de
+ * @author Florian Engel florian.engel@qaware.de
  */
 class MavenScanner : Scanner {
 
+    object MavenScannerConstants {
+        const val MAIN_SUBDIRECTORY: String = "src/main"
+        const val TEST_SUBDIRECTORY: String = "src/test"
+        const val JAVA_EXTENSION: String = ".java"
+    }
 
-    override fun scanCodebase(rootDir : Path) : Codebase {
+    override fun scanCodebase(rootDir: Path): Codebase {
 
-        val modules : List<Module> = Files.find(rootDir, 50, BiPredicate { path, attrs ->
-            attrs.isRegularFile && path.fileName.toString().equals("pom.xml") })
-                .filter { p -> !p.contains(Paths.get("target"))}
+        val modules: List<Module> = Files.find(rootDir, 50, BiPredicate { path, attrs ->
+            attrs.isRegularFile && path.fileName.toString().equals("pom.xml")
+        })
+                .filter { p -> !p.contains(Paths.get("target")) }
                 .map { p -> createModule(p) }
                 .collect(Collectors.toList<Module>())
 
-        return Codebase(rootDir, modules);
+        return Codebase(rootDir, modules)
     }
 
 
+    fun createModule(pathToPom: Path): Module {
 
+        val rootPath = pathToPom.parent
 
-    fun createModule(pathToPom : Path) : Module {
+        val mainPath = rootPath.resolve(MavenScannerConstants.MAIN_SUBDIRECTORY)
+        val testPath = rootPath.resolve(MavenScannerConstants.TEST_SUBDIRECTORY)
 
-        val rootPath = pathToPom.parent;
+        val mainFiles = findAllFilesRecursively(mainPath)
+        val testFiles = findAllFilesRecursively(testPath)
 
         return Module(rootPath.fileName.toString(),
                 rootPath,
-                emptyList(), // TODO scan for files
-                emptyList());
+                mainFiles,
+                testFiles)
     }
 
+    private fun findAllFilesRecursively(rootDir: Path): List<File> {
+        val paths = findAllFilePathsRecursively(rootDir)
+        return parseFiles(paths)
+    }
+
+    private fun parseFiles(filePaths: Iterable<Path>): List<File> {
+        return filePaths.map { p -> parseFile(p) }.toList()
+    }
+
+    private fun parseFile(filePath: Path): File {
+        val name = filePath.fileName.toString()
+        val type = getFileTypeFromFileName(name)
+        val entity = getEntity(filePath, type)
+        return File(filePath, name, entity, type)
+    }
+
+    private fun getEntity(filePath: Path, fileType: FileType): String {
+        if (fileType == FileType.JAVA) {
+            val inputStream = FileInputStream(filePath.toFile())
+            try {
+                val compilationUnit = JavaParser.parse(inputStream)
+                val classVisitor = ClassVisitor()
+                classVisitor.visit(compilationUnit, compilationUnit.`package`.packageName)
+                return classVisitor.fullyQualifiedClassName
+            } finally {
+                inputStream.close()
+            }
+        }
+        return filePath.fileName.toString()
+    }
+
+    private fun getFileTypeFromFileName(fileName: String): FileType {
+        if (fileName.toLowerCase().endsWith(MavenScannerConstants.JAVA_EXTENSION)) {
+            return FileType.JAVA
+        }
+        return FileType.OTHER
+    }
+
+    private fun findAllFilePathsRecursively(rootDir: Path): List<Path> {
+        val foundFiles = ArrayList<Path>()
+        if (Files.exists(rootDir)) {
+            val directoryStream = Files.newDirectoryStream(rootDir)
+            for (path in directoryStream) {
+                if (path.toFile().isDirectory) {
+                    foundFiles.addAll(findAllFilePathsRecursively(path))
+                } else {
+                    foundFiles.add(path)
+                }
+            }
+        }
+        return foundFiles
+    }
+
+    private class ClassVisitor : VoidVisitorAdapter<String>() {
+
+        var fullyQualifiedClassName: String = ""
+
+        override fun visit(n: ClassOrInterfaceDeclaration?, arg: String?) {
+            if (arg != null){
+                fullyQualifiedClassName = arg + "."
+            }
+            if (n != null) {
+                fullyQualifiedClassName += n.name
+            }
+            super.visit(n, arg)
+        }
+    }
 }
