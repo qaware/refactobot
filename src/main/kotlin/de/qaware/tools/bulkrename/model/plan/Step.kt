6e7a221323338f76cd10c1cecac655a8a0e3a6fe
Florian Engel <florian.engel@qaware.de>
2016-11-14 13:59:56 +0100

    Refines step definition.

--- src/main/kotlin/de/qaware/tools/bulkrename/model/plan/Step.kt
+++ src/main/kotlin/de/qaware/tools/bulkrename/model/plan/Step.kt
@@ -4,9 +4,20 @@ package de.qaware.tools.bulkrename.model.plan
  * A step in a refactoring plan.
  *
  * @author Alexander Krauss alexander.krauss@qaware.de
+ * @author Florian Engel florian.engel@qaware.de
  */
 data class Step (
 
-        val oldEntity : String
+        val oldModuleName: String,
+        val oldModulePath: String,
+        val oldFileName: String,
+        val oldFilePath: String,
+        val oldEntity : String,
+
+        val newModuleName: String,
+        val newModulePath: String,
+        val newFileName: String,
+        val newFilePath: String,
+        val newEntity : String
 
 )
