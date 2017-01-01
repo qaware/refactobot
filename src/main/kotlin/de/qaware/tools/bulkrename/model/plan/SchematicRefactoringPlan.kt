d1f21d4b3ba8a5297b241373f5c8c4ba5a819e59
Alexander Krauss <alexander.krauss@qaware.de>
2017-01-01 01:03:53 +0100

    Tuned docs.

--- src/main/kotlin/de/qaware/tools/bulkrename/model/plan/SchematicRefactoringPlan.kt
+++ src/main/kotlin/de/qaware/tools/bulkrename/model/plan/SchematicRefactoringPlan.kt
@@ -3,6 +3,9 @@ package de.qaware.tools.bulkrename.model.plan
 /**
  * Refactoring plan, consisting of rename steps.
  *
+ * Steps may contain wildcards, and a schematic plan is to be interpreted sequentially. That is, for each step, apply
+ * it to all matching files, and then proceed with the following steps.
+ *
  * @author Alexander Krauss alexander.krauss@qaware.de
  * @author Florian Engel florian.engel@qaware.de
  */
