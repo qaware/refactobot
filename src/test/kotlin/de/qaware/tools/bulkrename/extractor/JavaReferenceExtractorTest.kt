9d1ecbcfc00aaf4bc856ad9d4b86a8ff162d04d0
Alexander Krauss <alexander.krauss@qaware.de>
2017-01-02 14:36:14 +0100

    Restored test for reference extractor.

--- src/test/kotlin/de/qaware/tools/bulkrename/extractor/JavaReferenceExtractorTest.kt
+++ src/test/kotlin/de/qaware/tools/bulkrename/extractor/JavaReferenceExtractorTest.kt
@@ -2,9 +2,13 @@ package de.qaware.tools.bulkrename.extractor
 
 import de.qaware.tools.bulkrename.model.codebase.Codebase
 import de.qaware.tools.bulkrename.model.codebase.File
+import de.qaware.tools.bulkrename.model.reference.Location
+import de.qaware.tools.bulkrename.model.reference.Reference
+import de.qaware.tools.bulkrename.model.reference.ReferenceType
 import de.qaware.tools.bulkrename.scanner.MavenScanner
 import org.junit.Test
 import java.nio.file.Paths
+import kotlin.test.assertEquals
 import kotlin.test.fail
 
 /**
@@ -22,29 +26,29 @@ class JavaReferenceExtractorTest {
         val codebase = MavenScanner().scanCodebase(Paths.get(testFolderLocation.toURI()))
 
         // Extract references
-        val references = JavaReferenceExtractor().extractReferences(codebase)
+        val extractedReferences = JavaReferenceExtractor().extractReferences(codebase).toSet()
 
-        // Check if all references are identified correctly
+        // Expectation
         val interfaceA = getFileByEntity(codebase, "InterfaceA.java")
         val classA = getFileByEntity(codebase, "ClassA.java")
+        val classB = getFileByEntity(codebase, "ClassB.java")
+        val enumB = getFileByEntity(codebase, "EnumB.java")
         val testClassA = getFileByEntity(codebase, "TestClassA.java")
+        val testClassB = getFileByEntity(codebase, "TestClassB.java")
 
-//        assertEquals(2, references[interfaceA]!!.size)
-//        assertTrue { references[interfaceA]!!.containsAll(setOf(classA, testClassA)) }
-//
-//        val testClassB = getFileByEntity(codebase, "org.example.codebase.a.TestClassB")
-//        assertEquals(1, references[testClassB]!!.size)
-//        assertTrue { references[testClassB]!!.contains(testClassA) }
-//
-//        val enumB = getFileByEntity(codebase, "org.example.codebase.b.EnumB")
-//        val classB = getFileByEntity(codebase, "org.example.codebase.b.ClassB")
-//        assertEquals(1, references[enumB]!!.size)
-//        assertTrue { references[enumB]!!.contains(classB) }
-//
-//        // Check if no additional (and therefore false) references have been identified
-//        val filesWithoutIncomingReferences = references.filterKeys { !setOf(interfaceA, testClassB, enumB).contains(it) }
-//        assertTrue { filesWithoutIncomingReferences.filter { it.value.isNotEmpty() }.isEmpty() }
+        val expectedReferences = setOf(
+                Reference(classA, interfaceA, Location(3, 8), Location(3, 40), ReferenceType.IMPORT),
+                Reference(classA, interfaceA, Location(5, 32), Location(5, 41), ReferenceType.CLASS_OR_INTERFACE_REFERENCE),
+                Reference(classA, interfaceA, Location(7, 13), Location(7, 22), ReferenceType.CLASS_OR_INTERFACE_REFERENCE),
+                Reference(testClassA, testClassB, Location(2, 8), Location(2, 40), ReferenceType.IMPORT),
+                Reference(testClassA, testClassB, Location(6, 13), Location(6, 22), ReferenceType.CLASS_OR_INTERFACE_REFERENCE),
+                Reference(testClassA, interfaceA, Location(7, 13), Location(7, 45), ReferenceType.FQ_CLASS_OR_INTERFACE_REFERENCE),
+                Reference(classB, enumB, Location(5, 13), Location(5, 40), ReferenceType.FQ_CLASS_OR_INTERFACE_REFERENCE)
+        )
+
+        assertEquals(extractedReferences, expectedReferences);
     }
+
 
     private fun getFileByEntity(codebase: Codebase, fileName: String): File =
             codebase.modules.flatMap { it.sourceFolders }.flatMap { it.files }.find { it.fileName == fileName } ?: fail("Cannot find file.")
