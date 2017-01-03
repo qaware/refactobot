b873a406bc7456991cd29dc8bec58c0db1bea48b
Alexander Krauss <alexander.krauss@qaware.de>
2017-01-03 22:47:06 +0100

    Refactored test for reuse, added another simpler (and failing) test.

--- src/test/kotlin/de/qaware/tools/bulkrename/expander/SequentialExpanderTest.kt
+++ src/test/kotlin/de/qaware/tools/bulkrename/expander/SequentialExpanderTest.kt
@@ -16,58 +16,53 @@ import java.nio.file.Paths
  */
 class SequentialExpanderTest{
 
+    // Create two files, file1 should be matched and transformed, file 2 should not be matched.
+    val codebaseRoot = Paths.get("/home/tester/project/bulkrename/")
+    val sourceRoot = codebaseRoot.resolve("src/main/java")
+    val fullPath = sourceRoot.resolve("de/qaware/tools/bulkrename/test")
+
+    val file1 = File(fullPath, Paths.get("de/qaware/tools/bulkrename/test"), "TestDTO.java", FileType.JAVA)
+    val file2 = File(fullPath, Paths.get("de/qaware/tools/bulkrename/test"), "TestEntity.java", FileType.JAVA)
+
+    // The module foo, which contains the files
+    val moduleFoo = Module(name = "foo", modulePath = Paths.get("modules/foo"), sourceFolders = listOf(
+            SourceFolder("src/main/java", listOf(file1, file2))
+    ))
+    // An empty target module
+    val moduleFoobar = Module(name = "foobar", modulePath = Paths.get("modules/foobar"), sourceFolders = listOf(
+            SourceFolder("src/test/java", files = emptyList())
+    ))
+
+    val codebase = Codebase(rootPath = codebaseRoot,
+            modules = listOf(moduleFoo, moduleFoobar))
+
+
+
     @Test
     fun testExpandRefactoringPlan(){
 
-        // Create two steps which build upon each other
-        val sourceExpressions1 = mapOf(
-            Pair(RefactoringSubject.MODULE_NAME, Regex("foo")),
-            Pair(RefactoringSubject.FILE_NAME, Regex("([a-zA-Z]*)DTO.java")),
-            Pair(RefactoringSubject.FILE_PATH, Regex("""de\\qaware\\tools\\bulkrename\\([a-zA-Z\\]*)"""))
-        )
-        val targetExpressions1 = mapOf(
-                Pair(RefactoringSubject.MODULE_NAME, Regex("FOO")),
-                Pair(RefactoringSubject.FILE_NAME, Regex("$1Dto.java")),
-                Pair(RefactoringSubject.FILE_PATH, Regex("""de\\qaware\\tools\\bulkrename\\foobar\\$1"""))
-        )
-        val sourceExpressions2 = mapOf(
-                Pair(RefactoringSubject.MODULE_NAME, Regex("FOO")),
-                Pair(RefactoringSubject.MODULE_PATH, Regex("src/main/java")),
-                Pair(RefactoringSubject.FILE_NAME, Regex("([a-zA-Z]*)Dto.java")),
-                Pair(RefactoringSubject.FILE_PATH, Regex("""de\\qaware\\tools\\bulkrename\\foobar\\([a-zA-Z\\]*)"""))
-        )
-        val targetExpressions2 = mapOf(
-                Pair(RefactoringSubject.MODULE_NAME, Regex("foobar")),
-                Pair(RefactoringSubject.MODULE_PATH, Regex("src/test/java")),
-                Pair(RefactoringSubject.FILE_NAME, Regex("$1Bean.java")),
-                Pair(RefactoringSubject.FILE_PATH, Regex("""de\\qaware\\tools\\bulkrename\\foobar\\$1\\beans"""))
-        )
-        val steps = listOf(
-                Step(sourceExpressions1, targetExpressions1),
-                Step(sourceExpressions2, targetExpressions2)
-        )
-
-        val refactoringPlan = SchematicRefactoringPlan(steps)
-
-        // Create two files, file1 should be matched and transformed, file 2 should not be matched.
-        val codebaseRoot = Paths.get("/home/tester/project/bulkrename/")
-        val sourceRoot = codebaseRoot.resolve("src/main/java")
-        val fullPath = sourceRoot.resolve("de/qaware/tools/bulkrename/test")
-
-        val file1 = File(fullPath, Paths.get("de/qaware/tools/bulkrename/test"), "TestDTO.java", FileType.JAVA)
-        val file2 = File(fullPath, Paths.get("de/qaware/tools/bulkrename/test"), "TestEntity.java", FileType.JAVA)
-
-        // The module foo, which contains the files
-        val moduleFoo = Module(name = "foo", modulePath = Paths.get("modules/foo"), sourceFolders = listOf(
-                SourceFolder("src/main/java", listOf(file1, file2))
+        val refactoringPlan = SchematicRefactoringPlan(listOf(
+                Step(mapOf(
+                        Pair(RefactoringSubject.MODULE_NAME, Regex("foo")),
+                        Pair(RefactoringSubject.FILE_NAME, Regex("([a-zA-Z]*)DTO.java")),
+                        Pair(RefactoringSubject.FILE_PATH, Regex("""de\\qaware\\tools\\bulkrename\\([a-zA-Z\\]*)"""))
+                ), mapOf(
+                        Pair(RefactoringSubject.MODULE_NAME, Regex("FOO")),
+                        Pair(RefactoringSubject.FILE_NAME, Regex("$1Dto.java")),
+                        Pair(RefactoringSubject.FILE_PATH, Regex("""de\\qaware\\tools\\bulkrename\\foobar\\$1"""))
+                )),
+                Step(mapOf(
+                        Pair(RefactoringSubject.MODULE_NAME, Regex("FOO")),
+                        Pair(RefactoringSubject.MODULE_PATH, Regex("src/main/java")),
+                        Pair(RefactoringSubject.FILE_NAME, Regex("([a-zA-Z]*)Dto.java")),
+                        Pair(RefactoringSubject.FILE_PATH, Regex("""de\\qaware\\tools\\bulkrename\\foobar\\([a-zA-Z\\]*)"""))
+                ), mapOf(
+                        Pair(RefactoringSubject.MODULE_NAME, Regex("foobar")),
+                        Pair(RefactoringSubject.MODULE_PATH, Regex("src/test/java")),
+                        Pair(RefactoringSubject.FILE_NAME, Regex("$1Bean.java")),
+                        Pair(RefactoringSubject.FILE_PATH, Regex("""de\\qaware\\tools\\bulkrename\\foobar\\$1\\beans"""))
+                ))
         ))
-        // An empty target module
-        val moduleFoobar = Module(name = "foobar", modulePath = Paths.get("modules/foobar"), sourceFolders = listOf(
-                SourceFolder("src/test/java", files = emptyList())
-        ))
-
-        val codebase = Codebase(rootPath = codebaseRoot,
-                modules = listOf(moduleFoo, moduleFoobar))
 
         // Execute
         val actualResult = SequentialExpander(codebase).expandRefactoringPlan(refactoringPlan)
@@ -79,4 +74,28 @@ class SequentialExpanderTest{
 
         assertThat(actualResult).isEqualTo(expectedResult);
     }
+
+    @Test
+    fun testPlainRename(){
+
+        val refactoringPlan = SchematicRefactoringPlan(listOf(
+                Step(mapOf(
+                        Pair(RefactoringSubject.FILE_NAME, Regex("([a-zA-Z]*)DTO.java"))
+                ), mapOf(
+                        Pair(RefactoringSubject.FILE_NAME, Regex("$1Dto.java"))
+                ))
+        ))
+
+        // Execute
+        val actualResult = SequentialExpander(codebase).expandRefactoringPlan(refactoringPlan)
+
+        val expectedResult = mapOf(
+                Pair(file1, NewFileLocation(moduleFoobar, moduleFoobar.sourceFolders[0], Paths.get("de/qaware/tools/bulkrename/foobar/test/beans"), "TestBean.java")),
+                Pair(file2, NewFileLocation(moduleFoo, moduleFoo.sourceFolders[0], file2.path, file2.fileName)) // unchanged
+        )
+
+        assertThat(actualResult).isEqualTo(expectedResult);
+    }
+
+
 }
