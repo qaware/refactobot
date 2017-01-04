bce2cfe1a5f3516c404eeb01016e1ebaee1f46cd
Alexander Krauss <alexander.krauss@qaware.de>
2017-01-04 10:12:46 +0100

    Removed semicolon, which seems to type itself all the time;

--- src/main/kotlin/de/qaware/tools/bulkrename/executor/EditProcessor.kt
+++ src/main/kotlin/de/qaware/tools/bulkrename/executor/EditProcessor.kt
@@ -10,14 +10,14 @@ import java.io.Writer
  */
 class EditProcessor(input : List<String>, private val output: Writer) {
 
-    private val source = LineSource(input);
+    private val source = LineSource(input)
 
     fun applyEdits(edits: List<FileOperation.Edit>) {
 
         for ((span, replacementString) in edits.sortedBy { it.span.start }) {
 
             // copy over any material before the edit location
-            output.write(source.readUpTo(span.start));
+            output.write(source.readUpTo(span.start))
 
             // discard the text up to the end location, and write out the replacement instead
             source.readUpTo(span.end)
