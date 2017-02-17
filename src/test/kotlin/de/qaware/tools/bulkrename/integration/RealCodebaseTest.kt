f4f8c3b221e449574c994ce8ebf764dd4320f08b
Alexander Krauss <alexander.krauss@qaware.de>
2017-02-17 15:43:09 +0100

    Tuned build; more real-life testing.

--- src/test/kotlin/de/qaware/tools/bulkrename/integration/RealCodebaseTest.kt
+++ src/test/kotlin/de/qaware/tools/bulkrename/integration/RealCodebaseTest.kt
@@ -3,27 +3,50 @@ package de.qaware.tools.bulkrename.integration
 import de.qaware.tools.bulkrename.model.plan.RefactoringSubject
 import de.qaware.tools.bulkrename.model.plan.SchematicRefactoringPlan
 import de.qaware.tools.bulkrename.model.plan.Step
-import org.junit.Test
 import java.nio.file.Paths
 
+
 /**
- * Tests on real codebases.
- *
- * @author Alexander Krauss alexander.krauss@qaware.de
+ * Renames a few entities.
  */
-class RealCodebaseTest {
+val ENTITY_RENAME = SchematicRefactoringPlan(listOf(
+        renameStep(Regex("PSAProject\\.java"), "ProjectEntity.java"),
+        renameStep(Regex("PSASystemrelease\\.java"), "SystemreleaseEntity.java"),
+        renameStep(Regex("PSADomain\\.java"), "DomainEntity.java"),
+        renameStep(Regex("PSAQuestionnaire\\.java"), "QuestionnaireEntity.java")
+))
 
-    @Test
-    fun testPsa() {
+/**
+ * Renames all DaoBeans to DaoImpl
+ */
+val DAO_RENAME = SchematicRefactoringPlan(listOf(
+        renameStep(Regex("(.*)DaoBean\\.java"), "$1DaoImpl.java")
+))
 
-        BulkRenameTool.refactorMavenCodebase(Paths.get("P:/codebase2/code"),
-                SchematicRefactoringPlan(listOf(
-                        renameStep(Regex("(.*)DaoBean\\.java"), "$1DaoImpl.java")
-                )))
-    }
+/**
+ * Moves some DTOs to the respective business component.
+ */
+val DTO_MOVE = SchematicRefactoringPlan(listOf(
+        moveStep("Report(List)?DTO\\.java", "application/modules/psa_reports.api", "com/telekom/gis/psa/reports/api/dto")
+))
 
 
-    private fun renameStep(from: Regex, to: String) =
-            Step(mapOf(Pair(RefactoringSubject.FILE_NAME, Step.Replacement(from, to))))
+/**
+ * Main function to run the tool on a real codebase.
+ */
+fun main(args: Array<String>) {
 
+    BulkRenameTool.refactorMavenCodebase(Paths.get("P:/codebase2/code"), DTO_MOVE)
 }
+
+
+private fun renameStep(from: Regex, to: String) =
+        Step(mapOf(Pair(RefactoringSubject.FILE_NAME, Step.Replacement(from, to))))
+
+
+private fun moveStep(filenameRegex: String, newModule: String, newPath: String) =
+        Step(mapOf(
+                Pair(RefactoringSubject.FILE_NAME, Step.Replacement(Regex("^($filenameRegex)$"), "$1")),
+                Pair(RefactoringSubject.MODULE, Step.Replacement(Regex("^.*$"), newModule)),
+                Pair(RefactoringSubject.FILE_PATH, Step.Replacement(Regex("^.*$"), newPath))
+        ))
