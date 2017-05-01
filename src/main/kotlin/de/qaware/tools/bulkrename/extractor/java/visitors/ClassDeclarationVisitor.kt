86ab37d6e973adeb91bea9f8151aea4b406398eb
Alexander Krauss <alexander.krauss@qaware.de>
2017-05-01 23:01:11 +0200

    Properly handle enum declarations.

--- src/main/kotlin/de/qaware/tools/bulkrename/extractor/java/visitors/ClassDeclarationVisitor.kt
+++ src/main/kotlin/de/qaware/tools/bulkrename/extractor/java/visitors/ClassDeclarationVisitor.kt
@@ -2,12 +2,14 @@ package de.qaware.tools.bulkrename.extractor.java.visitors
 
 import com.github.javaparser.ast.CompilationUnit
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration
+import com.github.javaparser.ast.body.EnumDeclaration
+import com.github.javaparser.ast.body.TypeDeclaration
 import de.qaware.tools.bulkrename.extractor.java.JavaSimpleTypeReference
 import de.qaware.tools.bulkrename.extractor.java.ReferenceExtractionContext
 import de.qaware.tools.bulkrename.extractor.java.ReferenceVisitor
 
 /**
- * Visitor for class definitions.
+ * Visitor for class, interface and enum definitions.
  *
  * @author Alexander Krauss alexander.krauss@qaware.de
  */
@@ -15,12 +17,22 @@ class ClassDeclarationVisitor(context: ReferenceExtractionContext) : ReferenceVi
 
     override fun visit(decl: ClassOrInterfaceDeclaration?, arg: Unit) {
         if (decl != null) {
-            if (decl.parentNode is CompilationUnit) {
-                // we are the top-level type
-                emit(JavaSimpleTypeReference(context.getCurrentFile(), context.getCurrentFile(), decl.nameExpr.toSpan()))
-            }
+            visitTypeDeclaration(decl)
         }
         super.visit(decl, arg)
     }
 
+    override fun visit(decl: EnumDeclaration?, arg: Unit) {
+        if (decl != null) {
+            visitTypeDeclaration(decl)
+        }
+        super.visit(decl, arg)
+    }
+
+    private fun visitTypeDeclaration(decl: TypeDeclaration) {
+        if (decl.parentNode is CompilationUnit) {
+            // we are the top-level type
+            emit(JavaSimpleTypeReference(context.getCurrentFile(), context.getCurrentFile(), decl.nameExpr.toSpan()))
+        }
+    }
 }
---
+++ src/test/resources/rewrites/EnumRename.java
@@ -0,0 +1,15 @@
+package org.example.test.code;
+//
+// Simple rename of an enum.
+//
+//: org.example.test.code.TestEnum -> org.example.test.code.RenamedTestEnum
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+
+public enum /*|*/TestEnum/*->RenamedTestEnum*/ {
+
+    A, B, C;
+
+}
