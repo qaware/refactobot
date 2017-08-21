03e6821d8d6baab29e4bb3fc793c3b9e0310a301
Alexander Krauss <alexander.krauss@qaware.de>
2017-08-21 16:25:58 +0200

    Tuned doc, using kotlin conventions.

--- src/main/kotlin/de/qaware/refactobot/model/plan/FileLocation.kt
+++ src/main/kotlin/de/qaware/refactobot/model/plan/FileLocation.kt
@@ -4,29 +4,13 @@ package de.qaware.refactobot.model.plan
  * Data class for target names/locations with regard to a single file.
  *
  * @author Alexander Krauss alexander.krauss@qaware.de
+ *
+ * @param module the new module
+ * @param sourceFolder the new source folder
+ * @param path the new relative path, within the source folder
+ * @param fileName the new file name
  */
-data class FileLocation(
-
-        /**
-         * The new module.
-         */
-        val module: String,
-
-        /**
-         * The new source folder
-         */
-        val sourceFolder: String,
-
-        /**
-         * The new relative path, within the source folder.
-         */
-        val path: String,
-
-        /**
-         * The new filename
-         */
-        val fileName: String
-) {
+data class FileLocation(val module: String, val sourceFolder: String, val path: String, val fileName: String) {
 
     /**
      * The full path and name of the new file, relative to the codebase root.
