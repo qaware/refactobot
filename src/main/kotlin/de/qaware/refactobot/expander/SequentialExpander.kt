ecc2d0ba285b76443ea28111a7ed5df9719e22eb
Alexander Krauss <alexander.krauss@qaware.de>
2017-08-21 12:38:42 +0200

    Simplified.

--- src/main/kotlin/de/qaware/refactobot/expander/SequentialExpander.kt
+++ src/main/kotlin/de/qaware/refactobot/expander/SequentialExpander.kt
@@ -5,7 +5,6 @@ import de.qaware.refactobot.model.codebase.File
 import de.qaware.refactobot.model.plan.FileLocation
 import de.qaware.refactobot.model.plan.RefactoringStep
 import de.qaware.refactobot.util.slashify
-import java.util.*
 
 /**
  * The sequential expander creates a full refactoring plan from a schematic refactoring plan by applying the steps
@@ -17,7 +16,7 @@ import java.util.*
 class SequentialExpander(val codebase: Codebase) : Expander {
 
     override fun expandRefactoringPlan(refactoringPlan: List<RefactoringStep>): Map<File, FileLocation> {
-        val fullRefactoringPlan = HashMap<File, FileLocation>()
+        val fullRefactoringPlan = mutableMapOf<File, FileLocation>()
 
         // for each file in the codebase...
         for (module in codebase.modules) {
@@ -28,30 +27,21 @@ class SequentialExpander(val codebase: Codebase) : Expander {
                     val initialLocation = FileLocation(module.modulePath.slashify(), folder.path, file.path.slashify(), file.fileName)
                     val finalLocation = refactoringPlan.fold(initialLocation, { loc, step -> step(loc) })
 
+
+                    // ... check the result for validity
+                    val newModule = codebase.modules.find { it.modulePath.slashify() == finalLocation.module }
+                            ?: throw IllegalStateException("Unknown module " + finalLocation.module)
+
+                    if (newModule.sourceFolders.none { it.path == finalLocation.sourceFolder } ) {
+                        throw IllegalStateException("No source folder with path " + finalLocation.sourceFolder + " in module " + newModule.name)
+                    }
+
                     // ...and save the result as the final location of the file
-                    fullRefactoringPlan.put(file, createNewFileLocation(finalLocation))
+                    fullRefactoringPlan.put(file, finalLocation)
                 }
             }
         }
         return fullRefactoringPlan
-    }
-
-    private fun createNewFileLocation(expansionResult: FileLocation): FileLocation {
-
-        // TODO, this is now a mere check.
-
-        val fileName = expansionResult.fileName
-        val path = expansionResult.path
-        val moduleName = expansionResult.module
-        val sourceRoot = expansionResult.sourceFolder
-
-        val newModule = codebase.modules.find { it.modulePath.slashify() == moduleName }
-                ?: throw IllegalStateException("Unknown module " + moduleName)
-        val newSourceFolder = newModule.sourceFolders.find { it.path == sourceRoot }
-                ?: throw IllegalStateException("No source folder with path " + sourceRoot + " in module " + newModule.name)
-
-        return FileLocation(newModule.modulePath.slashify(), newSourceFolder.path, path, fileName)
-
     }
 
 }
