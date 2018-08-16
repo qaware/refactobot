86afc1fdb705b20caca03990346391152565612c
Alexander Krauss <alexander.krauss@qaware.de>
2018-08-16 16:50:53 +0200

    Added simple DSL method for applying a class map.

--- src/main/kotlin/de/qaware/refactobot/configuration/RefactoringContext.kt
+++ src/main/kotlin/de/qaware/refactobot/configuration/RefactoringContext.kt
@@ -1,6 +1,8 @@
 package de.qaware.refactobot.configuration
 
 import de.qaware.refactobot.model.plan.FileLocation
+import de.qaware.refactobot.util.classToFile
+import de.qaware.refactobot.util.fileToClass
 
 /**
  * Context for specifying a refactoring operation.
@@ -19,9 +21,22 @@ class RefactoringContext(originalLocation: FileLocation) {
         get() = FileLocation(module, sourceFolder, path, fileName)
 
 
-
+    /**
+     * DSL element for simple file rename
+     */
     fun renameFile(regex: String, newName: String) {
         fileName = fileName.replace(regex.toRegex(), newName)
+    }
+
+    /**
+     * DSL element for applying a class map
+     */
+    fun applyClassMap(classMap: Map<String, String>) {
+
+        val mappedClass = classMap.get(fileToClass(fileName))
+        if (mappedClass != null) {
+            fileName = classToFile(mappedClass)
+        }
     }
 
 
