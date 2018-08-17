2fa821dd24c5479fb9ab56aee85d616389462f50
Alexander Krauss <alexander.krauss@qaware.de>
2018-08-17 15:01:40 +0200

    More documentation.

--- src/test/kotlin/de/qaware/refactobot/integration/FullIntegrationTest.kt
+++ src/test/kotlin/de/qaware/refactobot/integration/FullIntegrationTest.kt
@@ -7,23 +7,38 @@ import org.junit.Test
 import org.junit.rules.TemporaryFolder
 import java.io.File
 
-
+/**
+ * Test for the complete tool, which can also serve as a starting point for your own applications.
+ *
+ * Copies some codebase into a temporary directory, runs the tool, and compares the outcome with an expected result.
+ */
 class FullIntegrationTest {
 
+    /**
+     * A temporary folder, for testing purposes
+     */
     @get:Rule
     val temporaryFolder = TemporaryFolder()
 
+    /**
+     * Source codebase.
+     */
     val sourceFolder = File(FullIntegrationTest::class.java.getResource("/integration_test/example_codebase").file)
+
+    /**
+     * Expected result, for checking correctness in the test.
+     */
     val expectedResult = File(FullIntegrationTest::class.java.getResource("/integration_test/expected_result").file)
 
     @Test
     fun test() {
 
+        // Set up temporary copy, to avoid messing with source files in a test
         val tmpDir = temporaryFolder.newFolder("test")
-
         FileUtils.copyDirectory(sourceFolder, tmpDir)
 
-        val bot = Refactobot.configure {
+        // Invoke configure and invoke the tool, with just two simple renames.
+        val refactobot = Refactobot.configure {
 
             refactor {
                 renameFile("InterfaceA.java" to "FancyInterface.java")
@@ -32,10 +47,10 @@ class FullIntegrationTest {
 
         }
 
-        bot.run(tmpDir.absolutePath)
-        
+        refactobot.run(tmpDir.absolutePath)
+
+        // Ensure that the result matches the expectations.
         DirectoryComparison.assertDirectoriesEqual(expectedResult, tmpDir)
-        
 
     }
 
